539c2b6b740efb126328947d0e127ba64eca2479
unknown <zhenyadope@gmail.com>
2020-11-27 01:45:04 +0300

    Секция PROJECT

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log(`Thanks for using my build ^-^`);
+// SWIPER SLIDER SETTING
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    centerMode:true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
