5e195854458c5dfe13e018ae56ddd04d26bcdb12
unknown <zhenyadope@gmail.com>
2020-12-02 14:19:29 +0300

    Добавлен адаптив и свойства слайдера

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
 // SWIPER SLIDER SETTING
 
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
+    
     spaceBetween: 30,
     freeMode: true,
     centerMode:true,
     loop: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      767:{
+        slidesPerView:1,
+        freeMode: false,
+    centerMode:false,
       },
+      1300:{
+        slidesPerView:3,
+      },
+      // 1400:{
+      //   slidesPerView:2,
+      // },
+      1540: {
+        slidesPerView:2,
+      },
+      1600:{
+        slidesPerView: 2.5,
+      },
+    },
+
 });
+
